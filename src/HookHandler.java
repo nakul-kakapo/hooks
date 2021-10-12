58623551ffc0216c1d88de64dffe916b2ceb233f
nakul-kakapo <91872193+nakul-kakapo@users.noreply.github.com>
2021-10-12 12:44:22 +0530

    Update HookHandler.java

--- src/HookHandler.java
+++ src/HookHandler.java
@@ -1,20 +1,30 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Executors;
 
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
+
 public class HookHandler implements HttpHandler {
-	
+
 	private static final int PORT = 4025;
 	private static final String CONTEXT = "/hooks";
-	
+	private static final String HUB_CHALLENGE = "hub.challenge";
+	private static final String HUB_MODE = "hub.mode";
+	private static final String HUB_VERIFY_TOKEN = "hub.verify_token";
+	private String challenge = "";
+
 	public HookHandler() {
 		run();
 	}
-	
+
 	public void run() {
 
 		// get page access token
@@ -30,9 +40,61 @@ public class HookHandler implements HttpHandler {
 		}
 	}
 
-	public void handle(HttpExchange exchange) throws IOException {
-		// TODO Auto-generated method stub
+	public void handle(HttpExchange httpExch) throws IOException {
 		
+		String query = httpExch.getRequestURI().getQuery();
+		System.out.println("Query: " + query);
+//		if (query != null) {
+//			Map<String, String> params = queryToMap(query);
+//			challenge = params.get(HUB_CHALLENGE);
+//		}
+//
+//		InputStream inputStream = httpExch.getRequestBody();
+//		StringBuilder sb = new StringBuilder();
+//		int i;
+//		while ((i = inputStream.read()) != -1) {
+//			sb.append((char) i);
+//		}
+//		inputStream.close();
+//		String incomingJSONContent = sb.toString();
+//		(new JsonParser()).parseJson(incomingJSONContent);
+//
+//		replyOK(httpExch); // SYNCHRONOUS REPLY TO FB
+//
+//		if (!VERIFY_WEBHOOK && !incomingJSONContent.contains("delivery") && !incomingJSONContent.contains("read")) {
+//			// get recipient ID from incoming message
+//			asyncReply(httpExch);
+//		}
+		 
+	}
+
+	private Map<String, String> queryToMap(String query) {
+		Map<String, String> result = new HashMap<>();
+		for (String param : query.split("&")) {
+			String[] entry = param.split("=");
+			if (entry.length > 1) {
+				result.put(entry[0], entry[1]);
+			} else {
+				result.put(entry[0], "");
+			}
+		}
+		return result;
+	}
+	
+	private void replyOK(HttpExchange httpExch) {
+		try {
+			Headers responseHeaders = httpExch.getResponseHeaders();
+			responseHeaders.add("Content-Type", ("application/json"));
+			httpExch.sendResponseHeaders(200, challenge.length());
+			try (OutputStream os = httpExch.getResponseBody()) {
+				if (!challenge.isEmpty()) {
+					os.write(challenge.getBytes());
+				}
+			}
+
+		} catch (IOException e1) {
+			System.out.println(e1.getMessage());
+		}
 	}
 
 }
